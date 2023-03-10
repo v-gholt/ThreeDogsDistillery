4d63ce310a9a678ba97be61e980057fb0d83c9a5
Geiselle Holt (Ada Developers Academy) <v-gholt@microsoft.com>
2023-03-10 10:17:58 -0800

    Test push

---
+++ Three-Dogs-Distillery-Backend-VS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Three_Dogs_Distillery_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
